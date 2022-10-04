f12f4cac89e2e07634eac6802609bd2abdfc909f
faisakhtar <faisakhtar@gmail.com>
2022-10-04 15:48:35 +0100

    Attach sample lambda layer

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Faisal, From Lambda... via Git!')
+        'body': json.dumps('Has the lambda layer attached?')
     }
